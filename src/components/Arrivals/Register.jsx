37950538915e7b56b98af6cb68085f2088092f66
Hiteshk369 <parchahiteshkumar123@gmail.com>
2023-10-10 12:24:06 +0530

    register form validation

--- src/components/Arrivals/Register.jsx
+++ src/components/Arrivals/Register.jsx
@@ -1,116 +1,184 @@
-import React,{useEffect,useState} from 'react'
-import Splash from '../../assets/images/splash.jpg'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import '../../assets/css/register.css'
-import axios from 'axios'
-import {useNavigate} from 'react-router-dom';
-const Register = () =>{
-    const navigate = useNavigate();
-    const navigateLogin = () => {
-      navigate('/login');
-    };
-    const navigateHome = () => {
-        navigate('/home');
-      };
-    
-    const[firstName,setFirstName] = useState('')
-    const[lastName,setLastName] = useState('')
-    const[password,setPassword] = useState('')
-    const[email,setEmail] = useState('')
-    const[phone,setPhone] = useState('')
-    async function signUp(){
-        let item={firstName,lastName,password,email,phone}
-         console.log(item)
-         axios.post('https://api-prana.prana24.in/api/auth/admin/signup',{
-            email:email,
-            first_name:firstName,
-            last_name:lastName,
-            phone_number:phone,
-            password:password
-         }).then(result=>{
-            console.log(result)
-         }).catch(error =>{
-            console.log(error)
-         })
-        // let result = await fetch('https://api-prana.prana24.in/api/auth/admin/signup',{
-        //     method:'POST',
-        //     body:JSON.stringify(item),
-        //     headers:{
-        //         "Content-Type" : 'application/json',
-        //         "Accept" : 'application/json'
-        //     }
-        // })
-        // result = await result.json()
-        // console.log("result",result)
-//         var myHeaders = new Headers();
-// myHeaders.append("Content-Type", "application/json");
+import React, { useEffect, useState } from "react";
+import Splash from "../../assets/images/splash.jpg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../../assets/css/register.css";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
-// var raw = JSON.stringify({
-//   "first_name": "J",
-//   "last_name": "K",
-//   "email": "grandhi.saikiranmayi@gmail.com",
-//   "password": "password",
-//   "phone_ext": "+91",
-//   "phone_number": "9100394566"
-// });
+const Register = () => {
+  const navigate = useNavigate();
+  const navigateLogin = () => {
+    navigate("/login");
+  };
+  const navigateHome = () => {
+    navigate("/home");
+  };
 
-// var requestOptions = {
-//   method: 'POST',
-//   headers: myHeaders,
-//   body: raw,
-//   redirect: 'follow'
-// };
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      phoneNumber: "",
+      password: "",
+    },
+  });
 
-// fetch("https://api-prana.prana24.in/api/auth/admin/signup", requestOptions)
-//   .then(response => response.text())
-//   .then(result => console.log(result))
-//   .catch(error => console.log('error', error));
-     }
-    return(
-        <div className='register-screen'>
-            <div className='d-flex flex-row col-lg-8'>
-                <div className='d-flex flex-column'>
-                    <img className='splash-img' src={Splash} alt="" />
-                </div>
-                <div className='d-flex flex-column col-lg-6'>
-                    <div className='outer'>
-                        <div className='inner'>
-                            <h3 className='prana-head'>Prana 24</h3>
-                            <form  className="form">
-                                <div className="form-group form-field">
-                                    <label>First Name</label>
-                                    <input type="text" className="form-control" placeholder="Enter First Name" name="first_name" value={firstName} onChange={(e) =>setFirstName(e.target.value)} />
-                                </div>
-                                <div className="form-group form-field">
-                                    <label>Last Name</label>
-                                    <input type="text" className="form-control" placeholder="Enter Last Name" name="last_name" value={lastName} onChange={(e) =>setLastName(e.target.value)} />
-                                </div>
-                                <div className="form-group form-field">
-                                    <label>Email</label>
-                                    <input type="email" className="form-control"  placeholder="Enter email" value={email} onChange={(e) =>setEmail(e.target.value)}/>   
-                                </div>
-                                <div className="form-group form-field">
-                                    <label>Phone No.</label>
-                                    <input type="Phone" className="form-control" placeholder="Enter contact no" value={phone} onChange={(e) =>setPhone(e.target.value)} />
-                                </div>
-                                <div className="form-group form-field">
-                                    <label>Password</label>
-                                    <input type="password" className="form-control" placeholder="Enter password" value={password}  onChange={(e) =>setPassword(e.target.value)}/>     
-                                </div>
-                                <div className='register-btn form-field'>
-                                    <button type="button" className='reg-btn' onClick={navigateHome}>
-                                        Register
-                                    </button>
-                                </div>
-                                <p className="forgot-password text-center for-par" onClick={navigateLogin}>
-                                    Already registered{"  "} <span className='log-login' >Login</span>    
-                                </p>   
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  const onSubmit = (data) => console.log(data);
+
+  // async function signUp(){
+  //     let item={firstName,lastName,password,email,phone}
+  //      console.log(item)
+  //      axios.post('https://api-prana.prana24.in/api/auth/admin/signup',{
+  //         email:email,
+  //         first_name:firstName,
+  //         last_name:lastName,
+  //         phone_number:phone,
+  //         password:password
+  //      }).then(result=>{
+  //         console.log(result)
+  //      }).catch(error =>{
+  //         console.log(error)
+  //      })
+  // let result = await fetch('https://api-prana.prana24.in/api/auth/admin/signup',{
+  //     method:'POST',
+  //     body:JSON.stringify(item),
+  //     headers:{
+  //         "Content-Type" : 'application/json',
+  //         "Accept" : 'application/json'
+  //     }
+  // })
+  // result = await result.json()
+  // console.log("result",result)
+  //         var myHeaders = new Headers();
+  // myHeaders.append("Content-Type", "application/json");
+
+  // var raw = JSON.stringify({
+  //   "first_name": "J",
+  //   "last_name": "K",
+  //   "email": "grandhi.saikiranmayi@gmail.com",
+  //   "password": "password",
+  //   "phone_ext": "+91",
+  //   "phone_number": "9100394566"
+  // });
+
+  // var requestOptions = {
+  //   method: 'POST',
+  //   headers: myHeaders,
+  //   body: raw,
+  //   redirect: 'follow'
+  // };
+
+  // fetch("https://api-prana.prana24.in/api/auth/admin/signup", requestOptions)
+  //   .then(response => response.text())
+  //   .then(result => console.log(result))
+  //   .catch(error => console.log('error', error));
+  //
+  return (
+    <div className="register-screen">
+      <div className="d-flex flex-row col-lg-8">
+        <div className="d-flex flex-column">
+          <img className="splash-img" src={Splash} alt="" />
         </div>
-    )
-}
-export default Register
+        <div className="d-flex flex-column col-lg-6">
+          <div className="outer">
+            <div className="inner">
+              <h3 className="prana-head">Prana 24</h3>
+              <form className="form" onSubmit={handleSubmit(onSubmit)}>
+                <div className="form-group form-field">
+                  <label>First Name</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Enter First Name"
+                    name="first_name"
+                    {...register("firstName", {
+                      required: true,
+                      minLength: 3,
+                      maxLength: 20,
+                    })}
+                    aria-invalid={errors.firstName ? "true" : "false"}
+                  />
+                  {errors.firstName?.type === "required" && (
+                    <p className="form-error" role="alert">
+                      First name is required
+                    </p>
+                  )}
+                  {errors.firstName?.type === "minLength" && (
+                    <p className="form-error" role="alert">
+                      Greater than 3 characters
+                    </p>
+                  )}
+                </div>
+                <div className="form-group form-field">
+                  <label>Last Name</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Enter Last Name"
+                    name="last_name"
+                    {...register("lastName", { pattern: /^[A-Za-z]+$/i })}
+                  />
+                </div>
+                <div className="form-group form-field">
+                  <label>Email</label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    placeholder="Enter email"
+                    {...register("email", {
+                      pattern:
+                        /^[a-zA-Z0-9._%+-]+a-@[zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                    })}
+                  />
+                </div>
+                <div className="form-group form-field">
+                  <label>Phone No.</label>
+                  <input
+                    type="Phone"
+                    className="form-control"
+                    placeholder="Enter contact no"
+                    {...register("phoneNumber", {
+                      pattern: /^\d{10}$/,
+                    })}
+                  />
+                </div>
+                <div className="form-group form-field">
+                  <label>Password</label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    placeholder="Enter password"
+                    {...register("password", {
+                      min: 8,
+                      pattern: /^(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.*\d).{8,}$/,
+                    })}
+                  />
+                </div>
+                <div className="register-btn form-field">
+                  <button type="submit" className="reg-btn">
+                    Register
+                  </button>
+                </div>
+                <p
+                  className="forgot-password text-center for-par"
+                  onClick={navigateLogin}
+                >
+                  Already registered{"  "}{" "}
+                  <span className="log-login">Login</span>
+                </p>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Register;
--- src/components/cart/index.jsx
+++ src/components/cart/index.jsx
@@ -1,21 +1,20 @@
 import React from "react";
 import Header from "../Home/Nav/header";
-import Cart from './cart'
+import Cart from "./cart";
 import Footer from "../Home/innerHome/footer";
-const CartIndex= () =>{
-    return(
-        <>
-            <div>
-                <Header />
-
-            </div>
-            <div>
-                <Cart />
-            </div>
-            <div>
-                <Footer />
-            </div>
-        </>
-    )
-}
-export default CartIndex
+const CartIndex = () => {
+  return (
+    <>
+      <div>
+        <Header />
+      </div>
+      <div>
+        <Cart />
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </>
+  );
+};
+export default CartIndex;

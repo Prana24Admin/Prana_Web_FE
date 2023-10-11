ea3ba1b651ea5734fc30769b9b5ec27b8a67cc2b
Hiteshk369 <parchahiteshkumar123@gmail.com>
2023-10-11 16:20:19 +0530

    category

--- src/components/HealthCare/Products/index.jsx
+++ src/components/HealthCare/Products/index.jsx
@@ -1,62 +1,50 @@
 import React from "react";
-import Carousel from "react-multi-carousel";
-import Card from "react-bootstrap/Card";
+
 import "react-multi-carousel/lib/styles.css";
-import Lupus from "../../../assets/images/health/lupus.jpg";
-import Digestive from "../../../assets/images/health/digestive.jpg";
-import Menstrual from "../../../assets/images/health/menstrual.jpg";
-import MothersDay from "../../../assets/images/health/mothersDay.jpg";
-import World from "../../../assets/images/health/world.jpg";
+
 // import "../../../assets/css/Health/healthCare.css";
 import "./products.css";
+import axiosInstance from "../../../libs/axios";
+import { useQuery } from "@tanstack/react-query";
 
-const products = [
-  {
-    id: 1,
-    image: World,
-    category: "Medicine",
-    discount: "30%",
-  },
-  {
-    id: 2,
-    image: Lupus,
-    category: "Lupus",
-    discount: "30%",
-  },
-  {
-    id: 3,
-    image: Digestive,
-    category: "general",
-    discount: "30%",
-  },
-  {
-    id: 4,
-    image: Menstrual,
-    category: "general",
-    discount: "30%",
-  },
-  {
-    id: 5,
-    image: MothersDay,
-    category: "general",
-    discount: "30%",
-  },
-];
 const Products = () => {
+  const fetchCategories = async () => {
+    const response = await axiosInstance.get("/filters?type=MEDICINE_CATEGORY");
+    return response.data;
+  };
+
+  const { data, isLoading, error } = useQuery(["categories"], fetchCategories);
+
   return (
     <>
       <div className="products-container">
-        <div className="products-box">
-          {/* <div className="card-card-innercared"> */}
-          {products.map((product) => (
-            <div className="products-card" id="card-health" key={product.id}>
-              <img className="products-image" src={product.image} alt="" />
-              <p className="products-title">{product.category}</p>
-              <p className="products-title">{product.discount}</p>
+        {isLoading && <p>Loading</p>}
+        {error && <p>Error fetching data! Try again</p>}
+        {data && (
+          <div className="products-marginContainer">
+            <div className="products-box">
+              {data.data.map((category) => (
+                <div key={category.id} className="products-card">
+                  <div className="products-innerContainer">
+                    <div>
+                      <img
+                        loading="lazy"
+                        src={category.image}
+                        width="90"
+                        height="90"
+                        alt={category.name}
+                        className="products-image"
+                      />
+                    </div>
+                    <div className="products-titleContainer">
+                      <p className="products-title">{category.name}</p>
+                    </div>
+                  </div>
+                </div>
+              ))}
             </div>
-          ))}
-          {/* </div> */}
-        </div>
+          </div>
+        )}
       </div>
     </>
   );

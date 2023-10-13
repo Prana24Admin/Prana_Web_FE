19b5e1938e1d3862b9517250dbcfe0e770a43711
Sanjay Kumar <sanjuchary5895@gmail.com>
2023-10-13 22:50:21 +0530

    Profile_address

--- src/components/Home/ProfileScreen/AddressScreen/AddressModal/index.jsx
+++ src/components/Home/ProfileScreen/AddressScreen/AddressModal/index.jsx
@@ -1,22 +1,39 @@
 import React from "react";
 import Modal from "react-bootstrap/Modal";
 import "./AddressModal.css";
+import Input from "../../../../Input";
 
 const AddressModal = ({ show, handleClose }) => {
   return (
-    <Modal show={show} onHide={handleClose} centered>
+    <Modal show={show} onHide={handleClose} centered size="lg">
       <Modal.Header closeButton>
-        <Modal.Title>Modal heading</Modal.Title>
+        <Modal.Title className="modal-title">Add an address</Modal.Title>
       </Modal.Header>
-      <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
-      <Modal.Footer>
-        <button className="modal-button" onClick={handleClose}>
-          Close
-        </button>
-        <button className="modal-button" onClick={handleClose}>
-          Save
-        </button>
-      </Modal.Footer>
+      <Modal.Body>
+        <div className="modal-bodyContainer">
+          <div className="modal-flexContainer">
+            <Input label="Name" />
+            <Input label="Phone number" />
+          </div>
+          <div className="modal-flexContainer">
+            <Input label="Street" />
+            <Input label="City" />
+          </div>
+          <div className="modal-flexContainer">
+            <Input label="State" />
+            <Input label="Pincode" />
+          </div>
+          <div className="modal-buttonsContainer">
+            <button className="modal-button" onClick={handleClose}>
+              Close
+            </button>
+            <button className="modal-button" onClick={handleClose}>
+              Save
+            </button>
+          </div>
+        </div>
+      </Modal.Body>
+      {/* <Modal.Footer></Modal.Footer> */}
     </Modal>
   );
 };

96bc430835ef0aef443f6df1ea3dac7c5655da4d
Hiteshk369 <parchahiteshkumar123@gmail.com>
2023-10-15 18:23:52 +0530

    order layout

---
+++ src/components/Home/Orders/OrdersScreen.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import OrderLayout from "./OrderLayout";
+
+const OrdersScreen = () => {
+  return (
+    <OrderLayout>
+      <p>All Orders</p>
+    </OrderLayout>
+  );
+};
+
+export default OrdersScreen;
--- src/components/Home/Orders/index.jsx
+++ src/components/Home/Orders/index.jsx
@@ -1,34 +1,17 @@
 import React from "react";
 import Header from "../Nav/nav";
 import InnerNav from "../Nav/innerNav";
+import Footer from "../innerHome/footer";
 import "./Orders.css";
+import OrdersScreen from "./OrdersScreen";
 
 const Orders = () => {
   return (
     <div>
       <Header />
       <InnerNav />
-      <div className="orders-mainContainer">
-        <div className="orders-container">
-          <div className="orders-leftContainer">
-            <p className="orders-Header">Filters</p>
-          </div>
-          <div className="orders-rightContainer">
-            <p className="orders-Header">Orders</p>
-            <div className="orders-flexContainer">
-              <img className="orders-image" src="" alt="img" />
-              <div className="orders-justifyContainer">
-                <div>
-                  <p className="orders-header">Item anme</p>
-                  <p className="orders-quantityText">Quantity:</p>
-                </div>
-                <div>₹ 3773</div>
-                <div>Rating</div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <OrdersScreen />
+      <Footer />
     </div>
   );
 };

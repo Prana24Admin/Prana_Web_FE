c6e9a71366a6a1e6d8329ac862dd83b47441307a
Sanjay Kumar <sanjuchary5895@gmail.com>
2023-10-16 13:40:06 +0530

    Orders Screnn

--- src/components/Home/Orders/OrdersScreen.jsx
+++ src/components/Home/Orders/OrdersScreen.jsx
@@ -1,10 +1,60 @@
 import React from "react";
 import OrderLayout from "./OrderLayout";
+import image from "../../../assets/images/profile/avatar.jpg";
 
 const OrdersScreen = () => {
   return (
     <OrderLayout>
-      <p>All Orders</p>
+      <div>
+        <p className="orders-Header">Your Orders</p>
+        <div className="orders-borderContainer">
+          <div className="orders-justifyContainer">
+            <div className="orders-flexContainer">
+              <div>
+                <p className="orders-text">Order Placed</p>
+                <p className="orders-date">Date</p>
+              </div>
+              <div>
+                <p className="orders-text">Total</p>
+                <p className="orders-date">₹200</p>
+              </div>
+              <div>
+                <p className="orders-text">Ship To</p>
+                <p className="orders-date">Address.name</p>
+              </div>
+            </div>
+            <div>
+              <div className="orders-idText">
+                <p>Order</p>
+                <p>#009900991-1000928-3110</p>
+              </div>
+              <div className="orders-bill">
+                <p>View order details</p>
+                <p>Invoice</p>
+              </div>
+            </div>
+          </div>
+          <div className="orders-details">
+            <div className="orders-detailsFlex">
+              <img className="orders-image" src={image} alt="sanju" />
+              <div className="orders-gapContainer">
+                <p className="orders-productName">Orders Name</p>
+                <button className="orders-button">View your Item</button>
+              </div>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                gap: "0.5rem",
+                flexDirection: "column",
+              }}
+            >
+              <button className="orders-buttonsRight">Customer support</button>
+              <button className="orders-buttonsRight">Write a review</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </OrderLayout>
   );
 };

ff0aa83a7c69a5a215604832bf90e4b2435a65e3
Sanjay Kumar <sanjuchary5895@gmail.com>
2023-10-23 20:56:16 +0530

    lab cart checkOut

--- src/pages/Home/Packages/LabTestCheckout/index.jsx
+++ src/pages/Home/Packages/LabTestCheckout/index.jsx
@@ -6,11 +6,60 @@ const LabTestCheckout = () => {
   return (
     <MainLayout>
       <div className="labCheckout-mainContainer">
-        <div>
-          <p>LEft</p>
-        </div>
-        <div>
-          <p>Right</p>
+        <div className="labCheckout-flexContainer">
+          <div className="labCheckout-leftContainer">
+            <p className="labCheckout-title">Payment Method</p>
+            <label className="labCheckout-gap">
+              <input type="radio" />
+              <p className="labCheckout-description">
+                Pay Cash at the time of sample collection{" "}
+              </p>
+            </label>
+            <label className="labCheckout-gap">
+              <input type="radio" />
+              <p className="labCheckout-description">
+                Pay Online at the time of sample collection{" "}
+              </p>
+            </label>
+          </div>
+          <div className="labCheckout-rightContainer">
+            <p className="labCheckout-title">Bill Summary</p>
+            <div className="labCheckout-justifyContainer">
+              <p className="labCheckout-description">Item Total</p>
+              <p className="labCheckout-mrp">
+                <span className="labCheckout-discount">₹123.00</span>
+                ₹123.00
+              </p>
+            </div>
+            <div className="labCheckout-justifyContainer">
+              <p className="labCheckout-description">Coupon Applied</p>
+              <p className="labCheckout-mrp">₹1234.00</p>
+            </div>
+            <div className="labCheckout-justifyContainer">
+              <p className="labCheckout-description">Home Collection Charges</p>
+              <p className="labCheckout-mrp">
+                <span className="labCheckout-discount">₹123.00</span>
+                FREE
+              </p>
+            </div>
+            <div className="labCheckout-line" />
+            <div className="labCheckout-justifyContainer">
+              <p
+                className="labCheckout-description"
+                style={{ fontWeight: "600" }}
+              >
+                Order Total
+              </p>
+              <p className="labCheckout-mrp">₹123.00</p>
+            </div>
+            <button className="cart-button">Proceed To Checkout</button>
+            <div className="cart-savingsContainer labCheckout-savingsContainer">
+              <p className="cart-savingsText">
+                Total savings of{" "}
+                <span style={{ fontWeight: "bold" }}>₹324</span> on this order
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </MainLayout>
